e39abaef249e4dc531544445ff876e2b2a045e20
Sean Moakes <mail@seanmoakes.com>
2025-01-31 16:44:33 +0000

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/seanmoakes/gator
+
+go 1.23.4
---
+++ internal/config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL       string `json:"db_url"`
+	CurrentUser string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	cfgPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(cfgPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	cfg := Config{}
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func (cfg Config) SetUser(username string) error {
+	cfg.CurrentUser = username
+	_ = write(cfg)
+	return nil
+}
+
+func write(cfg Config) error {
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path, data, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("could not write to file:%w", err)
+	}
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("unable to get the home directory")
+	}
+	return homeDir + "/" + configFileName, nil
+}
