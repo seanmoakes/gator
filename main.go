0dcfb4423431b63d65be11ee9bcc436ef99668b2
Sean Moakes <mail@seanmoakes.com>
2025-02-01 18:15:15 +0000

    register user command added

--- main.go
+++ main.go
@@ -1,30 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/seanmoakes/gator/internal/config"
+	"github.com/seanmoakes/gator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
+	// Read existing config
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	// Connect to database
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	// Set up program state
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
+	// register commands
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
