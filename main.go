04a2ad00753ae1da0338a76b4efd58c92149d21a
Sean Moakes <mail@seanmoakes.com>
2025-02-03 14:06:09 +0000

    browse posts command added

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerAddFollow))
 	cmds.register("following", middlewareLoggedIn(handlerGetFollowing))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,47 +1,33 @@
 -- name: CreateFeed :one
-INSERT INTO feeds (
-  id,
-  created_at,
-  updated_at,
-  name,
-  url,
-  user_id
-)
-VALUES (
-  $1,
-  $2,
-  $3,
-  $4,
-  $5,
-  $6
-)
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
--- name: GetFeed :one
-SELECT * FROM feeds
-WHERE name = $1;
+-- name: GetFeeds :many
+SELECT * FROM feeds;
 
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE url= $1;
 
+-- name: GetFeed :one
+SELECT * FROM feeds
+WHERE name = $1;
+
 -- name: GetFeedByID :one
 SELECT * FROM feeds
 WHERE id = $1;
 
--- name: GetFeeds :many
-SELECT * FROM feeds;
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
 
 -- name: MarkFeedFetched :one
 UPDATE feeds
 SET last_fetched_at = NOW(), updated_at = NOW()
 WHERE id = $1
 RETURNING *;
-
--- name: GetNextFeedToFetch :one
-SELECT * FROM feeds
-ORDER BY last_fetched_at ASC NULLS FIRST
-LIMIT 1;
 
 
 -- name: DeleteFeeds :exec
